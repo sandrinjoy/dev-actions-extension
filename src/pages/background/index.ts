202bcb7820d9fc56fcf392f0028d4d8d8d3e2700
Sandrin Joy <sandrinejoy007@gmail.com>
2023-11-26 16:30:51 +0530

    (feat): added sort tabs action

--- src/pages/background/index.ts
+++ src/pages/background/index.ts
@@ -1,1 +1,21 @@
-console.log('background script loaded');
+import { BACKGROUND_TASKS } from "@src/utils/constants";
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+	if (message.type === BACKGROUND_TASKS.SORT_TABS) {
+		chrome.tabs.query({ currentWindow: true }, (tabs) => {
+			const sortedTabs = tabs.sort((a, b) => {
+				// const aTitle = a?.title ?? '';
+				// const bTitle = b?.title ?? '';
+				// return aTitle.localeCompare(bTitle);
+				const aUrl = a?.url ?? "";
+				const bUrl = b?.url ?? "";
+				return aUrl.localeCompare(bUrl);
+			});
+			sortedTabs.forEach(async (tab, index) => {
+				const tabId = tab?.id ?? 0;
+				await chrome.tabs.move(tabId, { index });
+			});
+
+			sendResponse({ message: "success" });
+		});
+	}
+});
--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,39 +1,53 @@
-import React from 'react';
-import logo from '@assets/img/logo.svg';
-import { BACKGROUND_TASKS } from '@src/utils/constants';
-import { RiRestartLine } from 'react-icons/ri';
-import { on } from 'events';
+import React from "react";
+import logo from "@assets/img/logo.svg";
+import { BACKGROUND_TASKS } from "@src/utils/constants";
+import { RiRestartLine } from "react-icons/ri";
+import { GiStack } from "react-icons/gi";
+
 export default function Popup(): JSX.Element {
-  function handleRestartChrome(){
-    // this will simply load chrome://restart in a new tab
-    chrome.tabs.create({url: 'chrome://restart',
-    active: true,
-  }).then((tab) => {
-      console.log('tab', tab);
-    }
-    ).catch((err) => {
-      console.log('err', err);
-    }
-    );
-    
-   
+	function handleRestartChrome() {
+		// this will simply load chrome://restart in a new tab
+		chrome.tabs
+			.create({ url: "chrome://restart", active: true })
+			.then((tab) => {
+				console.log("tab", tab);
+			})
+			.catch((err) => {
+				console.log("err", err);
+			});
+	}
 
-  }
-
-  function handleSortTabs(){
-    // this will send a message to the background script
-    chrome.runtime.sendMessage({type: BACKGROUND_TASKS.SORT_TABS});
-  }
-  return (
-    <div className="absolute top-0 left-0 right-0 bottom-0 text-center h-full p-3 bg-gray-800">
-    <div className='flex gap-4 jsutify-center items-center'>
-     <div className='flex flex-col items-center justify-center aspect-square gap-1 h-16 text-white font-bold'>
-     <button onClick={handleRestartChrome} className='rounded-full bg-red-400 aspect-square h-10 flex items-center justify-center'>
-       <RiRestartLine className=' h-4 w-4'/>
-      </button>
-      Restart Chrome
-     </div>
-      </div>
-    </div>
-  );
+	function handleSortTabs() {
+		// this will send a message to the background script
+		chrome.runtime.sendMessage(
+			{ type: BACKGROUND_TASKS.SORT_TABS },
+			(response) => {
+				console.log("response", response);
+			}
+		);
+	}
+	return (
+		<div className="absolute top-0 left-0 right-0 bottom-0 text-center h-full p-3 bg-gray-800">
+			<div className="flex gap-4 jsutify-center items-center">
+				<div className="flex flex-col items-center justify-center aspect-square gap-1 h-16 text-white font-bold">
+					<button
+						onClick={handleRestartChrome}
+						className="rounded-full bg-red-400 aspect-square h-10 flex items-center justify-center"
+					>
+						<RiRestartLine className=" h-4 w-4" />
+					</button>
+					Restart Chrome
+				</div>
+				<div className="flex flex-col items-center justify-center aspect-square gap-1 h-16 text-white font-bold">
+					<button
+						onClick={handleSortTabs}
+						className="rounded-full bg-red-400 aspect-square h-10 flex items-center justify-center"
+					>
+						<GiStack className=" h-4 w-4" />
+					</button>
+					Sort
+				</div>
+			</div>
+		</div>
+	);
 }
